2574908ca55646a6296dba3264d5be223793272f
Prakshi <prakshisandhir37@gmail.com>
2025-08-12 19:08:37 +0530

    added code

--- Ceil_the_floor.py
+++ Ceil_the_floor.py
@@ -1,1 +1,24 @@
 #https://www.geeksforgeeks.org/problems/ceil-the-floor2802/1
+#User function Template for python3
+
+class Solution:
+    def getFloorAndCeil(self, x: int, arr: list) -> list:
+        arr.sort()
+        n = len(arr)
+        floor_val, ceil_val = -1, -1
+        low, high = 0, n - 1
+    
+        while low <= high:
+            mid = (low + high) // 2
+    
+            if arr[mid] == x:
+                return [arr[mid], arr[mid]]  
+            elif arr[mid] < x:
+                floor_val = arr[mid]        
+                low = mid + 1
+            else:
+                ceil_val = arr[mid]        
+                high = mid - 1
+    
+        return [floor_val, ceil_val]       
+        
