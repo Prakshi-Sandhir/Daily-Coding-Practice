90c5c51a975085bddc69e189656187fa8c217663
Prakshi <prakshisandhir37@gmail.com>
2025-08-18 11:05:39 +0530

    new file

---
+++ Max_Sum_Subarray_of_size_K.py
@@ -0,0 +1,14 @@
+# https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
+class Solution:
+    def maximumSumSubarray (self,arr,k):
+        n=len(arr)
+        current_element=0
+        last_element=k-1
+        window_sum=sum(arr[:k])
+        max_sum=window_sum
+        for i in range(n-k):
+            window_sum=window_sum-arr[i]+arr[i+k]
+            if max_sum<window_sum:
+                max_sum=window_sum
+        return max_sum
+                
