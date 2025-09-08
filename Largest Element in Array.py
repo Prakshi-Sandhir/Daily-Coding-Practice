961f2040a96326fe004faf9dd398e3fd19d74e23
Prakshi <prakshisandhir37@gmail.com>
2025-09-08 10:21:42 +0530

    new file

---
+++ Largest Element in Array.py
@@ -0,0 +1,44 @@
+# https://www.geeksforgeeks.org/problems/largest-element-in-array4009/1
+class Solution:
+    def largest(self, arr):
+        lar=arr[0]
+        for i in range(len(arr)-1):
+            if lar<arr[i+1]:
+                lar=arr[i+1]
+        return lar
+                    
+    
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+            
+                    
+                
+                
+        
